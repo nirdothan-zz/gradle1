87a9c4255e62630e809dbc788ca76842969e4b0e
Nir Dothan <nird@qwilt.com>
2017-11-12 11:44:33 +0200

    initial revision

---
+++ src/main/java/com/example/gradle1/Gradle1Application.java
@@ -0,0 +1,13 @@
+package com.example.gradle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradle1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(Gradle1Application.class, args);
+	}
+}
